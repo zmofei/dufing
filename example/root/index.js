5334add75ca24a4085fcf9eb39aed9ae56dbd01e
z.mofei <13761509829@163.com>
2016-08-05 23:15:12 +0800

    init

--- example/root/index.js
+++
@@ -1,9 +0,0 @@
-var render = function(req, res) {
-    this.jade({
-        data: {
-            title: 'abc'
-        }
-    })
-}
-
-module.exports = render;
--- example/root/test/index.js
+++
@@ -1,6 +0,0 @@
-exports.render = function(req, res){
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.end('Welcome to test\n');
-}
